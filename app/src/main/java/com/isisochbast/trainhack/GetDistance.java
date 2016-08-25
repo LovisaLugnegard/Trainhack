f329b68d0a2724eb10c52dc1102e2e341548e081
Lovisa Lugnegard <lovisa.lugnegard@gmail.com>
2016-08-25 10:39:37 +0200

    test

---
+++ app/src/main/java/com/isisochbast/trainhack/GetDistance.java
@@ -0,0 +1,103 @@
+package com.isisochbast.trainhack;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ * Created by Lovisa on 2016-08-25.
+ */
+public class GetDistance extends AsyncTask<String, Void, String> {
+
+    Context mContext;
+    Double duration;
+    Geo geo1;
+    //constructor is used to get the context.
+    public GetDistance(StartFragment mContext) {
+        //this.mContext = mContext;
+        geo1= (Geo) mContext;
+    }
+    //This function is executed before before "doInBackground(String...params)" is executed to dispaly the progress dialog
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+    //This function is executed after the execution of "doInBackground(String...params)" to dismiss the dispalyed progress dialog and call "setDouble(Double)" defined in "MainActivity.java"
+    @Override
+    protected void onPostExecute(String aDouble) {
+        super.onPostExecute(aDouble);
+        if(aDouble!=null)
+        {
+            geo1.setDouble(aDouble);
+        }
+        else
+            Toast.makeText(mContext, "Error4!Please Try Again wiht proper values", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        try {
+            URL url=new URL(params[0]);
+            HttpURLConnection con= (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.connect();
+            int statuscode=con.getResponseCode();
+            if(statuscode==HttpURLConnection.HTTP_OK)
+            {
+                BufferedReader br=new BufferedReader(new InputStreamReader(con.getInputStream()));
+                StringBuilder sb=new StringBuilder();
+                String line=br.readLine();
+                while(line!=null)
+                {
+                    sb.append(line);
+                    line=br.readLine();
+                }
+                String json=sb.toString();
+                Log.d("JSON",json);
+                JSONObject root=new JSONObject(json);
+                JSONArray array_rows=root.getJSONArray("rows");
+                Log.d("JSON","array_rows:"+array_rows);
+                JSONObject object_rows=array_rows.getJSONObject(0);
+                Log.d("JSON","object_rows:"+object_rows);
+                JSONArray array_elements=object_rows.getJSONArray("elements");
+                Log.d("JSON","array_elements:"+array_elements);
+                JSONObject  object_elements=array_elements.getJSONObject(0);
+                Log.d("JSON","object_elements:"+object_elements);
+               // JSONObject object_duration=object_elements.getJSONObject("duration");
+                JSONObject object_distance=object_elements.getJSONObject("distance");
+
+              //  Log.d("JSON","object_duration:"+object_duration);
+                return object_distance.getString("value");
+
+            }
+        } catch (MalformedURLException e) {
+            Log.d("error", "error1");
+        } catch (IOException e) {
+            Log.d("error", "error2");
+        } catch (JSONException e) {
+            Log.d("error","error3");
+
+        }
+
+
+        return null;
+    }
+    interface Geo{
+        public void setDouble(String min);
+    }
+
+}
--- app/src/main/java/com/isisochbast/trainhack/StartFragment.java
+++ app/src/main/java/com/isisochbast/trainhack/StartFragment.java
@@ -7,15 +7,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Lovisa on 2016-08-25.
  */
-public class StartFragment extends Fragment {
+public class StartFragment extends Fragment implements GetDistance.Geo{
 
     private EditText mStartEditStext;
     private EditText mSlutEditText;
     private Button mBerakna;
+    private String mStartDest;
+    private String mSlutDest;
+    private TextView mAvstancdTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -25,6 +29,32 @@ public class StartFragment extends Fragment {
         mSlutEditText = (EditText) view.findViewById(R.id.slut_editText);
         mBerakna = (Button) view.findViewById(R.id.berakna_button);
 
+        mBerakna.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mStartDest = mStartEditStext.getText().toString();
+                mSlutDest = mSlutEditText.getText().toString();
+
+
+                String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + mStartDest + "&destinations=" + mSlutDest + "&mode=driving&language=fr-FR&avoid=tolls&key=AIzaSyCCFP1Zdu51zwsF1x7mRlkRwYbxTwuqvdo";
+                new GetDistance(StartFragment.this).execute(url);
+            }
+
+        }
+
+        );
+
         return view;
     }
+
+    @Override
+    public void setDouble(String result) {
+        String res[]=result.split(",");
+        int dist=Integer.parseInt(res[1])/1000;
+        mAvstancdTextView.setText("Distance= " + dist + " kilometers");
+
+    }
+
+
+
 }
